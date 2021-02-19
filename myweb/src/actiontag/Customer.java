5b1a7063624f7051311ec8ede49726de7afbd81d
sw-Jack <jacksuwon@gmail.com>
2021-02-19 19:40:34 +0900

    JSP ActionTag & JavaBean 종합 예제

--- myweb/src/actiontag/Customer.java
+++ myweb/src/actiontag/Customer.java
@@ -1,6 +1,10 @@
 package actiontag;
 
-public class Customer {
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
 	private String name;
 	private String email;
 	private String phone;
