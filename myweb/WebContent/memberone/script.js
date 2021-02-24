e46763b83d11d0148d8560ec65e9635359ea8c0b
sw-Jack <jacksuwon@gmail.com>
2021-02-24 12:38:23 +0900

    회원가입 구현 - 자바 스크립트 함수

--- myweb/WebContent/memberone/script.js
+++ myweb/WebContent/memberone/script.js
@@ -24,3 +24,94 @@ function dongCheck() {
 	} 
 	document.zipForm.submit();
 }
+
+// 회원가입 시 모든 정보 입력 여부 체크
+function inputCheck() {
+	if(document.regForm.id.value == "") {
+		alert("아이디를 입력해주세요.");
+		document.regForm.id.focus();
+		return;
+	}
+	if(document.regForm.pass.value == "") {
+		alert("비밀번호를 입력해주세요.");
+		document.regForm.pass.focus();
+		return;
+	}
+	if(document.regForm.repass.value == "") {
+		alert("비밀번호를 확인해주세요.");
+		document.regForm.repass.focus();
+		return;
+	}
+	if(document.regForm.pass.value != document.regForm.repass.value) {
+		alert("비밀번호가 일치하지 않습니다.");
+		document.regForm.repass.focus();
+		return;
+	}
+	if(document.regForm.name.value == "") {
+		alert("이름을 입력해주세요.");
+		document.regForm.name.focus();
+		return;
+	}
+	if(document.regForm.phone1.value == "") {
+		alert("통신사를 입력해주세요.");
+		document.regForm.phone1.focus();
+		return;
+	}
+	if(document.regForm.phone2.value == "") {
+		alert("전화번호 앞자리를 입력해주세요.");
+		document.regForm.phone2.focus();
+		return;
+	}
+	if(document.regForm.phone3.value == "") {
+		alert("전화번호 뒷자리를 입력해주세요. ");
+		document.regForm.phone3.focus();
+		return;
+	}
+	if(document.regForm.email.value == "") {
+		alert("이메일을 입력해주세요.");
+		document.regForm.email.focus();
+		return;
+	}
+	
+	/*
+	var str = document.regForm.email.value;
+	var atPos = str.indexOf('@');
+	var atLastPos = str.lastIndindexOf('@');
+	var dotPos = str.indexOf('.');
+	var spacePos = str.indexOf(' ');
+	var commaPos = str.indexOf(',');
+	var emailSize = str.length;
+	if(atPos > 1 && atPos == atLastPos && dotPos > 3 && spacePos == -1 && commaPos == -1 && atPos + 1 < dotPos && dotPos + 1 < emailSize);
+	else {
+		alert('이메일 주소 형식이 잘못되었습니다\n\r다시 입력해주세요.');
+		document.regForm.email.focus();
+		return;
+	}
+	*/
+	
+	if(document.regForm.zipcode.value == "") {
+		alert("우편번호를 입력해주세요.");
+		document.regForm.zipcode.focus();
+		return;
+	}
+	if(document.regForm.address1.value == "") {
+		alert("주소를  입력해주세요.");
+		document.regForm.address1.focus();
+		return;
+	}
+	if(document.regForm.address2.value == "") {
+		alert("상세주소를 입력해주세요.");
+		document.regForm.address2.focus();
+		return;
+	}
+	document.regForm.submit();
+} 
+
+
+
+
+
+
+
+
+
