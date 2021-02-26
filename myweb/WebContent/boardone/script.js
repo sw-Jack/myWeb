b5d02e7c0c65ea06b136cee5e9ac4b2beb291569
sw-Jack <jacksuwon@gmail.com>
2021-02-26 23:50:48 +0900

    게시판 구현 - 자바 스크립트 함수

--- myweb/WebContent/boardone/script.js
+++ myweb/WebContent/boardone/script.js
@@ -1,3 +1,4 @@
+// 게시판 자바스크립트 함수
 // 글쓰기 입력란 체크 함수
 function writeSave() {
 	if(document.writeForm.writer.value == "") {
@@ -23,6 +24,15 @@ function writeSave() {
 	if(document.writeForm.pass.value == "") {
 		alert("비밀번호를 입력하세요.");
 		document.writeForm.pass.focus();
+		return false;
+	}
+}
+
+// 게시글 삭제 비밀번호 입력 체크 함수
+function deleteSave() {
+	if(document.delForm.pass.value == "") {
+		alert("비밀번호를 입력하세요.");
+		document.delForm.pass.focus();
 		return false;
 	}
 }
