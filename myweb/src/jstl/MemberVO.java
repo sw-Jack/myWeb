37bece25973c9255ca4e1d128fc8cce8cc3329d7
sw-Jack <jacksuwon@gmail.com>
2021-03-03 21:47:24 +0900

    jstl - MemberVO

---
+++ myweb/src/jstl/MemberVO.java
@@ -0,0 +1,39 @@
+package jstl;
+
+public class MemberVO {
+	private String name;
+	private String email;
+	private String age;
+	
+	public MemberVO() {}
+
+	public MemberVO(String name, String email, String age) {
+		this.name = name;
+		this.email = email;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+}
