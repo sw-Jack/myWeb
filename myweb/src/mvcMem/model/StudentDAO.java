5f1c7cbf754ae0a1377b3932dd65bdfb83900112
sw-Jack <jacksuwon@gmail.com>
2021-03-05 11:42:07 +0900

    MVC2 (model) - Student DAO

---
+++ myweb/src/mvcMem/model/StudentDAO.java
@@ -0,0 +1,251 @@
+package mvcMem.model;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Vector;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import memberone.StudentVO;
+
+public class StudentDAO {
+	private static StudentDAO instance = null;
+	private StudentDAO() {}
+	
+	public static StudentDAO getInstance() {
+		if(instance == null) {
+			synchronized (StudentDAO.class) {
+				instance = new StudentDAO();
+			}
+		}
+		return instance;
+	}
+	
+	private Connection getConnection() {
+		Connection conn = null;
+		try {
+			Context init = new InitialContext();
+			DataSource ds = (DataSource)init.lookup("java:comp/env/jdbc/myOracle");
+			conn = ds.getConnection();
+		} catch(NamingException e) {
+			System.err.println("Connection 생성 실패");
+			e.printStackTrace();
+		} catch(SQLException e) {
+			System.err.println("Connection 생성 실패");
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	public boolean idCheck(String id) {
+		boolean result = true;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement("select * from student where id = ?");
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if(!rs.next()) {
+				result = false;
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) try {rs.close();} catch(SQLException e) {}
+			if(pstmt != null) try {pstmt.close();} catch(SQLException e) {}
+			if(conn != null) try {conn.close();} catch(SQLException e) {}
+		}
+		return result;
+	}
+	
+	public boolean memberInsert(StudentVO vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		boolean flag = false; // 회원가입 실패
+		try {
+			conn = getConnection();
+			String strQuery = "insert into student values(?,?,?,?,?,?,?,?,?,?)";
+			pstmt = conn.prepareStatement(strQuery);
+			pstmt.setString(1, vo.getId());
+			pstmt.setString(2, vo.getPass());
+			pstmt.setString(3, vo.getName());
+			pstmt.setString(4, vo.getPhone1());
+			pstmt.setString(5, vo.getPhone2());
+			pstmt.setString(6, vo.getPhone3());
+			pstmt.setString(7, vo.getEmail());
+			pstmt.setString(8, vo.getZipcode());
+			pstmt.setString(9, vo.getAddress1());
+			pstmt.setString(10, vo.getAddress2());
+			int count = pstmt.executeUpdate();
+			if(count > 0) flag = true; // 회원가입 성공
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.err.println("Exception" + e);
+		} finally {
+			if(rs != null) try {rs.close();} catch(SQLException e) {}
+			if(pstmt != null) try {pstmt.close();} catch(SQLException e) {}
+			if(conn != null) try {conn.close();} catch(SQLException e) {}
+		}
+		return flag;
+	}
+	
+	public boolean updateMember(StudentVO vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean result = false;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement("update student set pass = ?, phone1 = ?, phone2 = ?, phone3 = ?, email = ?, zipcode = ?, address1 = ?, address2 = ? where id = ?");
+			pstmt.setString(1, vo.getPass());
+			pstmt.setString(2, vo.getPhone1());
+			pstmt.setString(3, vo.getPhone2());
+			pstmt.setString(4, vo.getPhone3());
+			pstmt.setString(5, vo.getEmail());
+			pstmt.setString(6, vo.getZipcode());
+			pstmt.setString(7, vo.getAddress1());
+			pstmt.setString(8, vo.getAddress2());
+			pstmt.setString(9, vo.getId());
+			if(pstmt.executeUpdate() > 0) result = true;
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(pstmt != null) try {pstmt.close();} catch(SQLException e) {}
+			if(conn != null) try {conn.close();} catch(SQLException e) {}
+		}
+		return result;
+	}
+	
+	public String getPass(String id) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String pass = null;
+		try {
+			conn = getConnection();
+			String strQuery = "select pass from student where id = ?";
+			pstmt = conn.prepareStatement(strQuery);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				pass = rs.getString("pass");
+			}
+		} catch(Exception e) {
+			System.out.println("Exception" + e); 
+		} finally {
+			if(rs != null) try {rs.close();} catch(SQLException e) {}
+			if(pstmt != null) try {pstmt.close();} catch(SQLException e) {}
+			if(conn != null) try {conn.close();} catch(SQLException e) {}
+		}
+		return pass;
+	}
+	
+	public int loginCheck(String id, String pass) {
+		String dbPass = getPass(id); // DB에 저장되어 있는 비밀번호
+		int check = -1; // 아이디가 없는 경우
+		if(dbPass != null) {
+			if(pass.equals(dbPass)) check = 1; // 로그인 성공
+			else check = 0; // 비밀번호 오류
+		}
+		return check; // 로그인 성공 여부 check 변수 return
+	}
+	
+	public int deleteMember(String id, String pass) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String dbPass = getPass(id); // DB에 저장된 비밀번호
+		int result = -1; // 결과치
+		try {
+			if (dbPass.equals(pass)) { // true - 본인 확인
+				conn = getConnection();
+				pstmt = conn.prepareStatement("delete from student where id = ?");
+				pstmt.setString(1, id);
+				pstmt.executeUpdate();
+				result = 1; // 회원탈퇴 성공
+			} else { // 본인 확인 실패 - 비밀번호 오류
+				result = 0;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) try {rs.close();} catch(SQLException e) {}
+			if(pstmt != null) try {pstmt.close();} catch(SQLException e) {}
+			if(conn != null) try {conn.close();} catch(SQLException e) {}
+		}
+		return result;
+	}
+	
+	public StudentVO getMember(String id) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StudentVO vo = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement("select * from student where id = ?");
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				vo = new StudentVO();
+				vo.setId(rs.getString("id"));
+				vo.setPass(rs.getString("pass"));
+ 				vo.setName(rs.getString("name"));
+				vo.setPhone1(rs.getString("phone1"));
+				vo.setPhone2(rs.getString("phone2"));
+				vo.setPhone3(rs.getString("phone2"));
+				vo.setEmail(rs.getString("email"));
+				vo.setZipcode(rs.getString("zipcode"));
+				vo.setAddress1(rs.getString("address1"));
+				vo.setAddress2(rs.getString("address2"));
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(rs != null) try {rs.close();} catch(SQLException e) {}
+			if(pstmt != null) try {pstmt.close();} catch(SQLException e) {}
+			if(conn != null) try {conn.close();} catch(SQLException e) {}
+		}
+		return vo; 
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
