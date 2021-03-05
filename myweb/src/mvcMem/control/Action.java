72b08e2949fae700128577f04594547c206e80e4
sw-Jack <jacksuwon@gmail.com>
2021-03-05 11:40:06 +0900

    Action Interface

---
+++ myweb/src/mvcMem/control/Action.java
@@ -0,0 +1,10 @@
+package mvcMem.control;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Action {
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException;
+}
