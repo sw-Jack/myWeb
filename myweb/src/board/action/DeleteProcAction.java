6c10483a9cebf88ab14192672423cd9706746217
sw-Jack <jacksuwon@gmail.com>
2021-03-07 19:53:51 +0900

    MVC2 게시판 구현 (Back Controller) - 게시글 삭제 페이지 처리 클래스

---
+++ myweb/src/board/action/DeleteProcAction.java
@@ -0,0 +1,24 @@
+package board.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import board.model.BoardDAO;
+
+public class DeleteProcAction implements CommandAction {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		request.setCharacterEncoding("utf-8");
+		int num = Integer.parseInt(request.getParameter("num"));
+		String pageNum = request.getParameter("pageNum");
+		String pass = request.getParameter("pass");
+		BoardDAO dbPro = BoardDAO.getInstance();
+		int check = dbPro.deleteArticle(num, pass);
+		// 해당 뷰에서 사용할 속성
+		request.setAttribute("pageNum", pageNum);
+		request.setAttribute("check", check);
+		return "/board/deleteProc.jsp"; // 해당 뷰
+	}
+
+}
