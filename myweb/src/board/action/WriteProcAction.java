348e8ead1baba93ef60301d46bfd2a11f4eb4338
sw-Jack <jacksuwon@gmail.com>
2021-03-07 19:55:14 +0900

    MVC2 게시판 구현 (Back Controller) - 게시글 작성 처리 클래스

---
+++ myweb/src/board/action/WriteProcAction.java
@@ -0,0 +1,32 @@
+package board.action;
+
+import java.sql.Timestamp;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import board.model.BoardDAO;
+import board.model.BoardVO;
+
+public class WriteProcAction implements CommandAction {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		request.setCharacterEncoding("utf-8");
+		BoardVO article = new BoardVO(); // 单捞磐 贸府 后
+		article.setNum(Integer.parseInt(request.getParameter("num")));
+		article.setWriter(request.getParameter("writer"));
+		article.setEmail(request.getParameter("email"));
+		article.setSubject(request.getParameter("subject"));
+		article.setPass(request.getParameter("pass"));
+		article.setRegdate(new Timestamp(System.currentTimeMillis()));
+		article.setRef(Integer.parseInt(request.getParameter("ref")));
+		article.setStep(Integer.parseInt(request.getParameter("step")));
+		article.setDepth(Integer.parseInt(request.getParameter("depth")));
+		article.setContent(request.getParameter("content"));
+		article.setIp(request.getRemoteAddr());
+		BoardDAO dbPro = BoardDAO.getInstance();
+		dbPro.insertArticle(article);
+		return "/board/writeProc.jsp";
+	}
+}
