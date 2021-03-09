db2db14dd89e2ca34db43098f56da5a1772564e0
sw-Jack <jacksuwon@gmail.com>
2021-03-09 13:08:25 +0900

    MVC2 게시판 구현 (Back Controller) - 게시글 작성 처리 클래스

--- myweb/src/board/action/WriteProcAction.java
+++ myweb/src/board/action/WriteProcAction.java
@@ -9,7 +9,6 @@ import board.model.BoardDAO;
 import board.model.BoardVO;
 
 public class WriteProcAction implements CommandAction {
-
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		request.setCharacterEncoding("utf-8");
